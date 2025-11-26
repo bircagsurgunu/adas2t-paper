cbbe025511d7cdd10d7007e215e7ff8f44f911a3
Samil <70058025+bircagsurgunu@users.noreply.github.com>
2025-11-26 11:19:37 +0300

    Delete deneme.py

--- deneme.py
+++
@@ -1,8 +0,0 @@
-from datasets import load_dataset
-from itertools import islice
-# Note this will take very long time to download and preprocess
-# you can try small subset for testing purpose
-ds =load_dataset("facebook/voxpopuli", "en_accented",split = "test")
-
-
-print(ds[0])
